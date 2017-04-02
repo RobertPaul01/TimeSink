55d56969ed2ee442d4aa58270dbc38b31dd6232b
Abe Ramseyer <abe.ramseyer@gmail.com>
2017-04-02 14:52:42 -0500

    Log in/ Log out works (tap number of minutes to log out)

--- app/src/main/java/com/abe/robert/timesink/LoginActivity.java
+++ app/src/main/java/com/abe/robert/timesink/LoginActivity.java
@@ -1,22 +1,8 @@
-/**
- * Copyright Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.abe.robert.timesink;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -49,7 +35,7 @@ public class LoginActivity extends AppCompatActivity implements
 
     private SignInButton mSignInButton;
 
-    private GoogleApiClient mGoogleApiClient;
+    public static GoogleApiClient mGoogleApiClient;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     // Firebase instance variables
