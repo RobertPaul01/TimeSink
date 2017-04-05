4837677e5b08547164c0cbabd50c27c698a5676e
Robby <rgilbertpaul@gmail.com>
2017-04-05 00:05:58 -0500

    Adding in bundle pass from MainActivity to ContentActivity with contentTime

--- app/src/main/java/com/abe/robert/timesink/ContentActivity.java
+++ app/src/main/java/com/abe/robert/timesink/ContentActivity.java
@@ -1,24 +1,53 @@
 package com.abe.robert.timesink;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+
 /**
+ * This activity is where the content will be displayed.
+ * Currently, this is extending YouTubeBaseActivity and only
+ * supporting YouTube videos. In order for these videos to work,
+ * the YouTube app must be installed on the device.
+ *
  * Created by Robby on 4/4/17.
  */
 
 public class ContentActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
-    public static final String YOUTUBE_API_KEY = "AIzaSyCqd9r0GUdd2RHttMvM5gnuguZSTjW7uaw";
+    private static final String TAG = "ContentActivity.java";
+
+    // From www.console.developers.google.com
+    public static final String YOUTUBE_API_KEY = "AIzaSyDp0k5y9Ru1GU7ftvlQ3jCVaxJjRQqmcWs";
+
+    // Duration of content to display
+    private int contentTime;
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_content);
+
+        // Catch the content time from MainActivity intent
+        Bundle b = getIntent().getExtras();
+        if (b != null) {
+            if (b.containsKey("CONTENT_TIME")) {
+                String time = (String) b.getCharSequence("CONTENT_TIME");
+                Log.i(TAG, "Bundle contains " + time + " in onCreate()");
+                this.contentTime = Integer.parseInt(time);
+            } else {
+                Log.e(TAG, "Bundle b does not contain CONTENT_TIME in onCreate()");
+            }
+        } else {
+            Log.e(TAG, "Bundle b is null in onCreate()");
+        }
 
         // Initialize the youtube view
         YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_player);
@@ -27,8 +56,7 @@ public class ContentActivity extends YouTubeBaseActivity implements YouTubePlaye
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-        Toast.makeText(ContentActivity.this, "Youtube initialization success", Toast.LENGTH_LONG).show();
-
+        Toast.makeText(ContentActivity.this, "Youtube initialization success. Need to look for content duration: " + contentTime + " mins.", Toast.LENGTH_LONG).show();
         youTubePlayer.loadVideo("dQw4w9WgXcQ");
     }
 
