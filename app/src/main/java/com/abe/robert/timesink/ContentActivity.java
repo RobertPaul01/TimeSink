c9904507a3e271664abad0266d34d07a27bffa50
Robby <rgilbertpaul@gmail.com>
2017-04-15 01:17:03 -0500

    Finished a good amount of the query logic. Going to need to work on a way to randomize the videos more since the Youtube api returns the most popular videos.

--- app/src/main/java/com/abe/robert/timesink/ContentActivity.java
+++ app/src/main/java/com/abe/robert/timesink/ContentActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -16,6 +17,9 @@ import android.widget.Toast;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * This activity is where the content will be displayed.
@@ -27,13 +31,23 @@ import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 
 public class ContentActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener {
 
+    // Logging tag
     private static final String TAG = "ContentActivity.java";
 
     // From www.console.developers.google.com
     public static final String YOUTUBE_API_KEY = "AIzaSyDp0k5y9Ru1GU7ftvlQ3jCVaxJjRQqmcWs";
 
-    // Duration of content to display
+    // Data from MainActivity bundle
     private int contentTime;
+    private ArrayList<String> queryTerms;
+
+    // Current video ID
+    private String currentId;
+
+    // UI views
+    private YouTubePlayer youTubePlayer;
+    private Button nextButton;
+    private Button prevButton;
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -41,19 +55,50 @@ public class ContentActivity extends AppCompatActivity implements YouTubePlayer.
         getSupportActionBar().hide();
         setContentView(R.layout.activity_content);
 
+        nextButton = (Button) findViewById(R.id.bt_next);
+        prevButton = (Button) findViewById(R.id.bt_prev);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadNextVideo();
+            }
+        });
+
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadPrevVideo();
+            }
+        });
+
         // Catch the content time from MainActivity intent
         Bundle b = getIntent().getExtras();
         if (b != null) {
             if (b.containsKey(MainActivity.CONTENT_TIME)) {
                 String time = (String) b.getCharSequence(MainActivity.CONTENT_TIME);
                 Log.i(TAG, "Bundle contains " + time + " in onCreate()");
-                this.contentTime = Integer.parseInt(time);
+                contentTime = Integer.parseInt(time);
             } else {
                 Log.e(TAG, "Bundle b does not contain " + MainActivity.CONTENT_TIME + " in onCreate()");
+            }
+
+            if (b.containsKey(MainActivity.CHECK_BOXES)) {
+                queryTerms = b.getStringArrayList(MainActivity.CHECK_BOXES);
+            } else {
+                Log.e(TAG, "Bundle b does not contain " + MainActivity.CHECK_BOXES + " in onCreate()");
             }
         } else {
             Log.e(TAG, "Bundle b is null in onCreate()");
         }
+
+        StringBuilder queryStr = new StringBuilder();
+        for (String term : queryTerms) {
+            queryStr.append(term).append("|");
+        }
+        queryStr.deleteCharAt(queryStr.length()-1);
+
+        ContentManager.getInstance().makeQuery(contentTime, queryStr.toString());
 
         // Initialize the YouTubePlayerSupportFragment
         YouTubePlayerSupportFragment youTubeFrag = (YouTubePlayerSupportFragment) getSupportFragmentManager().findFragmentById(R.id.youtube_fragment);
@@ -62,8 +107,26 @@ public class ContentActivity extends AppCompatActivity implements YouTubePlayer.
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-        Toast.makeText(ContentActivity.this, "Youtube initialization success. Need to look for content duration: " + contentTime + " mins.", Toast.LENGTH_LONG).show();
-        youTubePlayer.loadVideo("dQw4w9WgXcQ");
+        Log.d(TAG, "Youtube initialization success. Need to look for content duration: " + contentTime + " mins.");
+        this.youTubePlayer = youTubePlayer;
+        loadNextVideo();
+    }
+
+    private void loadNextVideo() {
+        String nextId = ContentManager.getInstance().getNextVideo(currentId);
+        if (nextId != null) {
+            currentId = nextId;
+            youTubePlayer.loadVideo(nextId);
+        }
+    }
+
+    private void loadPrevVideo() {
+        // If prevVideo id is null, nothing should happen.
+        String prevId = ContentManager.getInstance().getPrevVideo();
+        if (prevId != null) {
+            currentId = prevId;
+            youTubePlayer.loadVideo(prevId);
+        }
     }
 
     @Override
