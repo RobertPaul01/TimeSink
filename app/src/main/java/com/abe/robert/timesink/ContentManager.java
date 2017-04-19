068587802575695786c23fb84109506b03b89620
Robby <rgilbertpaul@gmail.com>
2017-04-19 01:10:18 -0500

    Fixed up some UI views

--- app/src/main/java/com/abe/robert/timesink/ContentManager.java
+++ app/src/main/java/com/abe/robert/timesink/ContentManager.java
@@ -233,10 +233,10 @@ public class ContentManager {
                 }).setYouTubeRequestInitializer(new YouTubeRequestInitializer(YOUTUBE_API_KEY)).setApplicationName("TimeSink").build();
 
                 // Define the API request for retrieving search results.
-                YouTube.Videos.List video = youtube.videos().list("id,snippet");
+                YouTube.Videos.List video = youtube.videos().list("id,snippet,contentDetails");
 
                 video.setId(vidId);
-                video.setFields("items(id,snippet)");
+                video.setFields("items(id,snippet,contentDetails)");
                 VideoListResponse videoResponse = video.execute();
 
                 List<Video> searchResultList = videoResponse.getItems();
@@ -248,7 +248,9 @@ public class ContentManager {
                 if (searchResultList != null) {
                     for (Video searchResult : searchResultList) {
                         VideoSnippet snip = searchResult.getSnippet();
-                        VideoData nD = new VideoData(searchResult.getId(), snip.getTitle(), snip.getDescription());
+                        String durationStr = searchResult.getContentDetails().getDuration();
+                        String min = durationStr.substring(2, durationStr.indexOf('M')) + " mins.";
+                        VideoData nD = new VideoData(searchResult.getId(), snip.getTitle(), min);
                         likedVideoData.add(nD);
                     }
                 }
